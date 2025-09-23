44d5defe9872d8dc8758359904643ae139292e98
Muhammad <muminmuhsina1@gmail.com>
2025-09-23 07:40:43 -0700

    Refractor: Add home and about routes
    Add home and about routes using the Link from next/link and nest them in the Header page which is in the index page

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div>
       <h1>This is the header section</h1>
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
     </div>
   );
 };
+
+export default Header;
