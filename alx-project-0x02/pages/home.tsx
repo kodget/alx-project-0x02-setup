2e5f07d85abbfbd4cff6cc7f3e5d0a52c4924f8c
Muhammad <muminmuhsina1@gmail.com>
2025-09-24 07:30:07 -0700

    .

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,9 @@
-import CardList from "@/components/common/CardList";
+import Card from "@/components/common/CardList";
 const Home = () => {
   return (
     <>
       <h1>This is the Home page</h1>
-      <CardList />
+      <Card />
     </>
   );
 };
