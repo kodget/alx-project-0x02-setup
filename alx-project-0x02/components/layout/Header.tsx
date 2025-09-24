3c243cef3b0ad6cdc246ff4a84a113340cf3f2b7
Muhammad <muminmuhsina1@gmail.com>
2025-09-24 07:19:14 -0700

    Commit messages are herculean to write

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -5,8 +5,12 @@ const Header = () => {
     <div>
       <h1>This is the header section</h1>
       <div className="grid grid-cols-2 place-content-between">
-        <Link href="/home">Home</Link>
-        <Link href="/about">About</Link>
+        <Link className="justify-start" href="/home">
+          Home
+        </Link>
+        <Link className="justify-end" href="/about">
+          About
+        </Link>
       </div>
     </div>
   );
