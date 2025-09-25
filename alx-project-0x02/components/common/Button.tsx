6339ce6510f87a4eae2ea51654c3f6879cb57dbd
Muhammad <muminmuhsina1@gmail.com>
2025-09-25 05:11:04 -0700

    Refractor: make little changes to button component

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 const Button: React.FC<ButtonProps> = ({ size, shape, title }) => {
   return (
     <button className={`p-4 bg-green-800 text-white ${size} ${shape}`}>
